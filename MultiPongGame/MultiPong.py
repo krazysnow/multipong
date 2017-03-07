bccea269fcabd7b0c6a5a42b58dfb14b04da0ce7
krazysnow <krazysnowdev@gmail.com>
2017-03-07 12:59:50 -0600

    Center text
    Centered the text, and added a commented resizing

--- MultiPongGame/MultiPong.py
+++ MultiPongGame/MultiPong.py
@@ -1,12 +1,14 @@
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.widget import Widget
+#from kivy.core.window import Window
 
 
 class MultiPong(App):
     def build(self):
         top = Widget()
-        top.add_widget(Label(text='Hello there\nnanners city!'))
+        top.add_widget(Label(center=top.center, text='Hello there\nnanners city!'))
+        #Window.size = top.children[0].size
         return top
 
 if __name__ == "__main__":
